4d2a317071795dc08c23840627f3e665f7058b8a
Chinmay Navale <chinmaymnavale@gmail.com>
2020-12-20 14:25:08 +0530

    Setting up the authentiction route using express.Router

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+const authRoutes = require('./routes/auth');
 
 mongoose
   .connect(process.env.DATABASE, {
@@ -12,8 +17,13 @@ mongoose
   })
   .then(() => console.log('DB CONNECTED'));
 
-const port = process.env.PORT || 8000;
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
+app.use('/api', authRoutes);
+
+const port = process.env.PORT || 8000;
 app.listen(port, () =>
   console.log(`Server is up and running on port ${port}...`)
 );
---
+++ controllers/auth.js
@@ -0,0 +1,3 @@
+exports.signup = (req, res) => res.json({ message: 'sign up route works!' });
+
+exports.signout = (req, res) => res.json({ message: 'User signed out' });
