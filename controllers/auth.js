a382bcd37571316774a98e69d538a0ab35d0731a
Chinmay Navale <chinmaymnavale@gmail.com>
2020-12-23 16:29:56 +0530

    Implementing auth controller for Saving a user to database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,16 @@
-exports.signup = (req, res) => res.json({ message: 'sign up route works!' });
+const User = require('../models/user');
+
+exports.signup = (req, res) => {
+  const user = new User(req.body);
+
+  user.save((error, user) => {
+    if (error) return res.status(400).json({ error: 'Not able to save in DB' });
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id,
+    });
+  });
+};
 
 exports.signout = (req, res) => res.json({ message: 'User signed out' });
